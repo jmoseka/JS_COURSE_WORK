73fea171b0c770a0dec132d4d165312f241a2f28
jmoseka <jamylam3@gmail.com>
2021-12-21 14:27:21 +0300

    using functions

--- 02-Fundamentals/01_function.js
+++ 02-Fundamentals/01_function.js
@@ -1,16 +1,14 @@
-function calcAverage (a, b, c)
-{
-    (a+b+c)/3;
-}
+const calcAverage  = (a, b, c) => (a+b+c)/3;
 function checkWinner(a, b){
     a > (b*2) ?
     console.log(`Dolphin wins (${a} vs ${b}😎`)
     : b > (a*2) ? console.log(`Koala wins (${b} vs ${a}😎`)
     : console.log(`No winner 😪`)
 }
-let avgDolphin = calcAverage(44, 23, 71);
-let avgsKoala = calcAverage(65, 54, 49);
+let avgDolphin = calcAverage(85, 54, 41);
+let avgsKoala = calcAverage(23, 34, 27);
 
+console.log(avgDolphin, avgsKoala);
 checkWinner(avgDolphin, avgsKoala);
 
 
