886a43cbf3e38c37c86e9cefef81ccd6b0463450
jmoseka <jamylam3@gmail.com>
2021-12-21 19:00:07 +0300

    function as property

--- 02-Fundamentals/03_object.js
+++ 02-Fundamentals/03_object.js
@@ -1,19 +1,24 @@
-const jonas = {
-    firstName: 'Jonas',
-    lastName: 'Schmedtmann',
-    birthYeah: 1991,
-    job: 'teacher',
-    friends: ['Michael', 'Peter', 'Steven'],
-    hasDriversLicense: false,
-    calcAge: function () {
-        this.age = 2037 - this.birthYeah;
-        return this.age;
-      },
-    getSummary: function() {
-        return `Jonas is ${this.age} and ${this.hasDriversLicense? "has driving license" : "does not have a driving license"}`;
+const mark = {
+    fullName: "Mark Miller",
+    mass: 78,
+    height: 1.69,
+    calcBMI: function(){
+        this.BMI = (this.mass) / (this.height * this.height);
+        return this.BMI;
     }
 };
 
-console.log(jonas.calcAge());
-console.log(jonas.getSummary());
-console.log(jonas.age);
+const john = {
+    fullName: "John Smith",
+    mass: 92,
+    height: 1.95,
+    calcBMI: function(){
+        this.BMI = (this.mass) / (this.height * this.height);
+        return this.BMI;
+    }
+};
+mark.calcBMI();
+john.calcBMI();
+
+console.log(mark.BMI > john.BMI  ? `Mark's BMI's (${mark.BMI}) is  higher than  John's`
+: `John's BMI's (${john.BMI}) is  higher than Mark`);
