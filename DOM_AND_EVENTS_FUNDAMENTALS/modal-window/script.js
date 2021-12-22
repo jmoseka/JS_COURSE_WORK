51cbc65dfa3791ef10db8514a4cc4a20771df2a7
jmoseka <jamylam3@gmail.com>
2021-12-22 21:07:56 +0300

    added keyboard click listeners

--- DOM_AND_EVENTS_FUNDAMENTALS/modal-window/script.js
+++ DOM_AND_EVENTS_FUNDAMENTALS/modal-window/script.js
@@ -9,18 +9,22 @@ const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 for (let i = 0; i < btnOpenModal.length; i++) {
   //anonymous function
-  btnOpenModal[i].addEventListener("click", function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-
-    //modal.style.display = 'block';
-  });
+  btnOpenModal[i].addEventListener("click", openModal);
 }
 
 btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-overlay.addEventListener("click", function () {
-  closeModal();
+document.addEventListener("keydown", function (e) {
+  //console.log(e.key);
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
 });
