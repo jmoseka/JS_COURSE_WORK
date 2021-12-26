b4cb3640c4988825e863ff36cf354f256bc1f83f
jmoseka <jamylam3@gmail.com>
2021-12-26 17:46:29 +0300

    optional chaining operators

---
+++ DS-MODERN-OPERATORS-STRINGS/optional_chaining.js
@@ -0,0 +1,15 @@
+//arrays
+const users = [{ name: "Jonas", email: "jonas.gmail", age: 12 }];
+
+//checks if users has a value at index 0
+//if there is, moves to the next instruction, if not prints "not available"
+//console.log(users[1]?.nam ?? "not available");
+
+//objects
+const person = {
+  users,
+  order(i, j) {
+    console.log(`Order is ${i} and also ${j}`);
+  },
+};
+console.log(person.order?.(0, 1) ?? "Method not available");
