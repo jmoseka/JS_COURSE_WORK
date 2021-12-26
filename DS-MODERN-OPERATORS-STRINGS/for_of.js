0bfc627fa32f0aa5ad03aee1b4250489e8d2b6bd
jmoseka <jamylam3@gmail.com>
2021-12-26 16:38:46 +0300

    for off loop and destructuring

---
+++ DS-MODERN-OPERATORS-STRINGS/for_of.js
@@ -0,0 +1,52 @@
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//1
+const [player1, player2] = game.players;
+
+for (const item of player1) {
+  console.log(item);
+}
+///console.log(player1.entries());
+console.log(...player1.entries());
+for (const [index, name] of player1.entries()) {
+  console.log(index, name);
+}
