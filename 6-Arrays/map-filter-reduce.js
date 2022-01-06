8641cde81f3cdac74980117914808f1152d7fb00
jmoseka <jamylam3@gmail.com>
2022-01-06 17:39:15 +0300

    map-reduce-reduce theory more in the bankist app

---
+++ 6-Arrays/map-filter-reduce.js
@@ -0,0 +1,16 @@
+/**
+ * *
+ * Map returns a NEW ARRAY containing the results of applying an operational on
+ * all original array elements
+ * current * 2
+ *
+ * Filter - returns a NEW ARRAY containing the array elements
+ * that passed a speicified test condition
+ * current > 2
+ *
+ * Reduce - boils(reduces) all array elements to a single value
+ * i.e adding all elements together
+ * acc + current
+ * - keeps getting bigger and bigger (snowball effect)
+ *
+ */
