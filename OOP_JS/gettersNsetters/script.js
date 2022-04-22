a32f548c2222225138f84572031af3d818d212e4
jmoseka <jamylam3@gmail.com>
2022-04-22 12:21:14 +0300

    add get and set methods

--- OOP_JS/gettersNsetters/script.js
+++ OOP_JS/gettersNsetters/script.js
@@ -1,28 +1,32 @@
-'use strict'
-
-class Person{
-    constructor(fullName, birthYear)
-    {
-        this.fullName = fullName;
-        this.birthYear = birthYear;
-    }
-
-    get age() {
-        return 2020 - this.birthYear;
-    }
-
-    set fullName(name) {
-        if (name.length < 5){
-            this._fullName = name;
-        }  
-        else{
-            console.log("invalid full names")
-        }  
-    }
+// Class declaration
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+  // Instance methods
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+  get age() {
+    return 2037 - this.birthYear;
+  }
+  // Set a property that already exists
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else console.log(`${name} is not a full name!`);
+  }
+  get fullName() {
+    return this._fullName;
+  }
+  
 }
-
-//const Jessica = new Person('Jessica', 1995)
-//console.log(Jessica.age);
-
-const Mweni = new Person('Jessica Mweni', 1995)
-console.log(Mweni)
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
+console.log(jessica.__proto__ === PersonCl.prototype);
