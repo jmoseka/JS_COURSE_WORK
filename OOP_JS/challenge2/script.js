a438cfd948c929924de352d88c1b904911b2bf72
jmoseka <jamylam3@gmail.com>
2022-04-22 16:01:00 +0300

    add challenge 2

---
+++ OOP_JS/challenge2/script.js
@@ -0,0 +1,31 @@
+class Car{
+    constructor(make, speed){
+            this.make = make;
+            this.speed = speed;
+    }
+
+
+    get speedUS(){
+        return this.speed/1.6;
+    }
+    set speedUS(speed) {
+        this.speed = speed * 1.6;
+    }
+
+    accelerate(){
+        this.speed += 10;
+        console.log(`${this.make} going at ${this.speed} km/h`)
+    }
+    
+    brake(){
+        this.speed -= 5;
+        console.log(`${this.make} going at ${this.speed} km/h`)
+    
+    }
+}
+
+const ford = new Car('Ford', 120);
+console.log('speed us' + ford.speedUS)
+ford.accelerate();
+ford.speedUS = 50;
+console.log(ford);
